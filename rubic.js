3828e0c1fc066e9fc2f102ad621bddd148d770ba
Farbod Aprin <fa@rias-institute.eu>
2021-03-06 18:04:15 +0100

    fix the array asignment

--- rubic.js
+++ rubic.js
@@ -1,7 +1,7 @@
 const { doDuring } = require("async");
 const { SSL_OP_SINGLE_DH_USE } = require("constants");
 
-console.log("Hello Rubik !");
+console.log("Hello Object orented Rubik !");
 
 function Rubic(up, down, right, left, front, back, Name, rubicSize) {
   // Note: Don't worry about 'this' yet. You'll understand it later. Follow along for now.
@@ -13,26 +13,52 @@ function Rubic(up, down, right, left, front, back, Name, rubicSize) {
   this.left = left;
   this.front = front;
   this.back = back;
-  function changeforSurfaceColor(input1, input2, input3, input4) {
-    temp = input1;
+  // function changeforSurfaceColor(input1, input2, input3, input4) {
+  //   temp = input1;
+  //   for (let i = 0; i < 3; i++) {
+  //     temp = input4[i];
+  //     input4[i] = input3[i];
+  //     input3[i] = input2[i];
+  //     input2[i] = input1[i];
+  //     input1[i] = temp;
+  //   }
+  // }
+
+  function changeforSurfaceColor2(input1, input2, input3, input4) {
+    const temp1 = input1.map((x) => x);
+    const temp2 = input2.map((x) => x);
+    const temp3 = input3.map((x) => x);
+    const temp4 = input4.map((x) => x);
     for (let i = 0; i < 3; i++) {
-      temp = input4[i];
-      input4[i] = input3[i];
-      input3[i] = input2[i];
-      input2[i] = input1[i];
-      input1[i] = temp;
+      input2[i] = temp1[i];
+      input3[i] = temp2[i];
+      input4[i] = temp3[i];
+      input1[i] = temp4[i];
     }
   }
+
   this.moveUp = function () {
-    changeforSurfaceColor(back, right, front, left);
+    changeforSurfaceColor2(back, right, front, left);
   };
   this.moveUpCC = function () {
-    changeforSurfaceColor(right, front, left, back);
+    changeforSurfaceColor2(right, back, left, front);
   };
-  this.moveUp = function () {
+  this.moveRight = function () {
     changeforSurfaceColor(back, right, front, left);
   };
-  this.moveUp = function () {
+  this.moveRightCC = function () {
+    changeforSurfaceColor(back, right, front, left);
+  };
+  this.moveLeft = function () {
+    changeforSurfaceColor(back, right, front, left);
+  };
+  this.moveLeftCC = function () {
+    changeforSurfaceColor(back, right, front, left);
+  };
+  this.moveForward = function () {
+    changeforSurfaceColor(back, right, front, left);
+  };
+  this.moveForwardCC = function () {
     changeforSurfaceColor(back, right, front, left);
   };
 
@@ -66,14 +92,13 @@ const defult = new Rubic(
   ["g", "g", "g", "g", "g", "g", "g", "g", "g"],
   ["w", "w", "w", "w", "w", "w", "w", "w", "w"],
   ["y", "y", "y", "y", "y", "y", "y", "y", "y"],
-
   "defult rubic",
   9
 );
 
 //defult.NewRubic();
 //defult.printRubic();
-defult.moveUp();
+
 defult.printRubic();
 /**
  * 
